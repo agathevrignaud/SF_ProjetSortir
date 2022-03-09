6fe10dd2fc135cf22c884fcdbcc82bf44b15ded4
avrignaud <vrignaud.agathe@gmail.com>
2022-03-09 10:59:59 +0100

    Added logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,9 +24,10 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
