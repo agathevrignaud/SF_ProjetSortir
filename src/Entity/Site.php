ac07db9472bf62d8975d1a0033ac4e48920337c6
Morgane <morgane.leden@gmail.com>
2022-03-10 16:40:37 +0100

    formulaire pour site et ville

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SiteRepository::class)]
+#[UniqueEntity(fields: ['nom'], message: 'Ce site exsite déjà')]
 class Site
 {
     #[ORM\Id]
