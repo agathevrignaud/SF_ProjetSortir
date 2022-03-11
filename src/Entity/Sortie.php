92382702c3fa86362169856b978d66c026303ad5
avrignaud <vrignaud.agathe@gmail.com>
2022-03-11 18:24:39 +0100

    Divided activity insert/update and cancel

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -52,6 +52,9 @@ class Sortie
     #[ORM\JoinColumn(nullable: false)]
     private $lieu;
 
+    #[ORM\Column(type: 'string', length: 100, nullable: true)]
+    private $motifAnnulation;
+
 
     public function __construct()
     {
@@ -199,6 +202,18 @@ class Sortie
     public function setLieu(?Lieu $lieu): self
     {
         $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getMotifAnnulation(): ?string
+    {
+        return $this->motifAnnulation;
+    }
+
+    public function setMotifAnnulation(?string $motifAnnulation): self
+    {
+        $this->motifAnnulation = $motifAnnulation;
 
         return $this;
     }
