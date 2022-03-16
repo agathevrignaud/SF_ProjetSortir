e11558df1837976a84d260b65fe183b7a06a010c
Morgane <morgane.leden@gmail.com>
2022-03-16 15:53:58 +0100

    ajout des validations sur user

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -162,7 +162,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('morgane.leden@gmail.com', 'Mailer Bot'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
