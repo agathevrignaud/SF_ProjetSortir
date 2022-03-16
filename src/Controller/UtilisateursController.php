f54886bcaa47653af6288c49edb0d131183a6f4c
avrignaud <vrignaud.agathe@gmail.com>
2022-03-16 15:26:09 +0100

    added mail notification + trip cancel to user activate/deactivate

--- src/Controller/UtilisateursController.php
+++ src/Controller/UtilisateursController.php
@@ -3,19 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\EtatRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateursController extends AbstractController
 {
     #[Route('/admin/utilisateurs', name: 'utilisateurs')]
-    public function afficherUtilisateurs(EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository): Response
+    public function afficherUtilisateurs(Request $request, UserRepository $userRepository): Response
     {
         /*
          * Form Filtre sur le prénom/nom des utilisateurs
@@ -51,13 +55,68 @@ class UtilisateursController extends AbstractController
     }
 
     #[Route('/admin/utilisateurs/{id}', name: 'utilisateur_actif_inactif')]
-    public function activerUtilisateur(int $id,EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository): Response
+    public function activerUtilisateur(int $id,EntityManagerInterface $entityManager, UserRepository $userRepository, EtatRepository $etatRepository, MailerInterface $mailer): Response
     {
         $user = $userRepository->find($id);
         $user->setActif(!$user->getActif());
 
         $entityManager->persist($user);
         $entityManager->flush();
+
+        if (!$user->getActif()) {
+            /*
+             * Annulation des sorties dont $user est organisateur
+             * & annulation de son inscription à des sorties
+             */
+            foreach ($user->getSortiesOrganisateur() as $sortie) {
+                if ( $sortie->getEtat()->getLibelle() =='Clôturée' ) {
+                    $sortie->setMotifAnnulation('Annulation par action administrateur.');
+                    $etat = $etatRepository->findOneBy(['libelle' => 'Annulée']);
+                    $sortie->setEtat($etat);
+
+                    /*
+                     * Notification des participants par email
+                     */
+                    $to = array();
+                    foreach ($sortie->getSortiesParticipants() as $participant) {
+                        $to[] = Address::create($participant->getPrenom() .'<'.$participant->getEmail().'>');
+                    }
+                    $email = (new TemplatedEmail())
+                        ->from(Address::create('sortir.com <sortir@no-reply.com>'))
+                        ->to(...$to)
+                        ->subject('Sortie Annulée : '.$sortie->getNom())
+                        ->htmlTemplate('pages/emails/annulationSortie.html.twig')
+                        ->context([
+                            'sortie' => $sortie,
+                        ])
+                    ;
+                    $mailer->send($email);
+
+                    $entityManager->persist($sortie);
+                    $entityManager->flush();
+                }
+            }
+            foreach ($user->getParticipant() as $sortie) {
+                $sortie->removeSortiesParticipant($user);
+                $entityManager->persist($sortie);
+                $entityManager->flush();
+            }
+        }
+
+        /*
+        * L'utilisateur est prévenu par email de l'état de son compte
+        */
+        $etatCompte = $user->getActif() ? "réactivé" : 'désactivé';
+        $email = (new TemplatedEmail())
+            ->from(Address::create('sortir.com <sortir@no-reply.com>'))
+            ->to(Address::create($user->getPrenom() .'<'.$user->getEmail().'>'))
+            ->subject('Votre compte a été '.$etatCompte)
+            ->htmlTemplate('pages/emails/etatCompte.html.twig')
+            ->context([
+                'user' => $user,
+            ])
+        ;
+        $mailer->send($email);
 
         $this->addFlash(
             'notice',
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -231,7 +231,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function removeParticipant(Sortie $participant): self
     {
         $this->participant->removeElement($participant);
-
         return $this;
     }
 
