3fbaf39bd19aefde90228421a4d7a0b3dd1a753e
avrignaud <vrignaud.agathe@gmail.com>
2022-03-17 12:39:14 +0100

    fixed form

--- src/Form/EditPasswordType.php
+++ src/Form/EditPasswordType.php
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ProfilFormType extends AbstractType
+class EditPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
